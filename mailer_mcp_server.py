6032a3301eed05fd885a82fe1dc01d3512c4a4e2
Dotdrelle <46851844+dotdrelle@users.noreply.github.com>
2026-07-10 22:21:09 +0200

    release: 0.13.0

--- mailer_mcp_server.py
+++ mailer_mcp_server.py
@@ -31,7 +31,7 @@ import uvicorn
 
 app = Server("agent-mailer-api")
 
-_AGENT_VERSION = "0.12.12"
+_AGENT_VERSION = "0.13.0"
 _MCP_TOKEN = os.environ.get("MCP_AUTH_TOKEN", "")
 _MCP_READ_TOKEN = os.environ.get("MCP_READ_TOKEN", "")
 _MCP_WRITE_TOKEN = os.environ.get("MCP_WRITE_TOKEN", "")
